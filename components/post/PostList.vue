d0f2292dd98db5c9a85da1ecda24cc2357f350b7
kartalc <81776410+kartalc@users.noreply.github.com>
2021-04-19 16:38:08 +0200

    erster commit

---
+++ components/post/PostList.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="container-fluid mt-5 d-flex flex-wrap justify-content-center align-items-center">
+        <Post v-for="index in 5" :key='index' />  
+    </div>
+</template>
+<script>
+    import Post from "@/components/post/Post"
+    export default {
+        components : {
+            Post
+        }
+}
+</script>
